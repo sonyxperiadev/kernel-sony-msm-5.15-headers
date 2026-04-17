88bd88916aec39b596761cfbfc39f7132b96b19e
Pavel Dubrova <pashadubrova@gmail.com>
2026-04-17 09:24:11 +0300

    cp_headers: Identify lunch target by string
    Any platform using the corresponding version of the Linux
    kernel is suitable, as opposed to specifying a number,
    which can point to a completely different target depending
    on changes in the environment.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- cp_headers.sh
+++ cp_headers.sh
@@ -93,7 +93,7 @@ HEADER_OVERRIDES="\
 cd ../../../..
 
 source build/envsetup.sh
-lunch 50
+lunch aosp_xqdq54-trunk_staging-userdebug
 
 for x in $LINUX_HEADERS; do \
 cp $HEADER_SRC/$x $HEADER_ORI/$x
